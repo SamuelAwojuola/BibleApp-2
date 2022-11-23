29ac7c98ccc63178c2381d618b83d5d64e4311ac
SamAwo <55980014+SamuelAwojuola@users.noreply.github.com>
2022-11-23 16:14:12 +0100

    Update bibleApp_createDownloadFile.js

--- script.js/bibleApp_createDownloadFile.js
+++ script.js/bibleApp_createDownloadFile.js
@@ -52,6 +52,7 @@ function readFromVerseNotesFiles(bookName, chapternumber, verseNumber) {
     }
 
     async function fetchBookNotes() {
+        console.log('fetching note')
         const response = await fetch(`/bible_notes/notes_${bookName}.json`);
         return await response.json()
     }

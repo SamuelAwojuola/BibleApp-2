49bd0dca71ad08c742ec6255a15b00baa3d53612
SamAwo <55980014+SamuelAwojuola@users.noreply.github.com>
2022-12-28 14:42:50 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ console.log('WORKER: executing.');
 /* A version number is useful when updating the worker logic,
    allowing you to remove outdated cache entries during the update.
 */
-var version = 'v3::';
+var version = 'v4::';
 
 /* These resources will be downloaded and cached by the service worker
    during the installation process. If any resource fails to be downloaded,
@@ -13,8 +13,8 @@ var version = 'v3::';
 */
 var offlineFundamentals = [
    '',
-   '/bibleApp.css',
-   '/bibleApp-2.css'
+   'bibleApp.css',
+   'bibleApp-2.css'
 //,     '/page2_versenotes.css',
 //     '/images/hamburger-menu-svgrepo-com.svg',
 //     '/images/scroll-svgrepo-com.svg',

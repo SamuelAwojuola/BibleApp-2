fdea1773aaaef9c456e345b510cc6461a853f939
SamAwo <55980014+SamuelAwojuola@users.noreply.github.com>
2022-12-28 12:55:23 +0100

    service worker code enabled

--- script.js/bibleApp.js
+++ script.js/bibleApp.js
@@ -1,14 +1,14 @@
 // ServiceWorker is a progressive technology. Ignore unsupported browsers
-// if ('serviceWorker' in navigator) {
-//     console.log('CLIENT: service worker registration in progress.');
-//     navigator.serviceWorker.register('/service-worker.js').then(function () {
-//         console.log('CLIENT: service worker registration complete.');
-//     }, function () {
-//         console.log('CLIENT: service worker registration failure.');
-//     });
-// } else {
-//     console.log('CLIENT: service worker is not supported.');
-// }
+if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register('/service-worker.js').then(function () {
+        console.log('CLIENT: service worker registration complete.');
+    }, function () {
+        console.log('CLIENT: service worker registration failure.');
+    });
+} else {
+    console.log('CLIENT: service worker is not supported.');
+}
 
 //GENERATE ALL CHAPTERS IN A BOOK ON DOUBLE-CLICKING IT
 /* document.addEventListener("dblclick", function (e) {

013f0fcf722897c9e4b8f8d85a13656bb5aebe73
SamAwo <55980014+SamuelAwojuola@users.noreply.github.com>
2023-01-06 06:07:15 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,41 @@
-"use strict";
+// "use strict";
 
-console.log('WORKER: executing.');
+// console.log('WORKER: executing.');
 
 /* A version number is useful when updating the worker logic,
    allowing you to remove outdated cache entries during the update.
 */
-var version = 'v6::';
+var version = 'v1::';
 
 /* These resources will be downloaded and cached by the service worker
    during the installation process. If any resource fails to be downloaded,
    then the service worker won't be installed either.
 */
 var offlineFundamentals = [
-   ''
-//   'bibleApp.css',
-//   'bibleApp-2.css',
-//   'page2_versenotes.css',
-//     '/images/hamburger-menu-svgrepo-com.svg',
-//     '/images/scroll-svgrepo-com.svg',
-//     '/images/settings-gear-svgrepo-com.svg',
-//     '/images/search-svgrepo-com (2).svg',
-//     '/images/books-stack-svgrepo-com.svg',
-//     '/images/search-svgrepo-com(2).svg',
-//     '/images/book-pencil-svgrepo-com.svg',
-//     '/images/settings-svgrepo-com.svg',
-//     '/images/home-svgrepo-com(maroon).svg',
-//     '/images/arrow-up-svgrepo-com.svg',
-//     '/images/arrow-down-svgrepo-com.svg',
-//   'page2_versenotes.html'
+    '',
+    'bibleApp.css',
+    'bibleApp-2.css',
+    'page2_versenotes.css',
+    'images/hamburger-menu-svgrepo-com.svg',
+    'images/scroll-svgrepo-com.svg',
+    'images/settings-gear-svgrepo-com.svg',
+    'images/search-svgrepo-com (2).svg',
+    'images/books-stack-svgrepo-com.svg',
+    'images/search-svgrepo-com(2).svg',
+    'images/book-pencil-svgrepo-com.svg',
+    'images/settings-svgrepo-com.svg',
+    'images/home-svgrepo-com(maroon).svg',
+    'images/arrow-up-svgrepo-com.svg',
+    'images/arrow-down-svgrepo-com.svg',
+    'page2_versenotes.html'
 ];
 
 /* The install event fires when the service worker is first installed.
    You can use this event to prepare the service worker to be able to serve
    files while visitors are offline.
 */
-console.log(offlineFundamentals);
 self.addEventListener("install", function(event) {
-  console.log('WORKER: install event in progress.');
+  // console.log('WORKER: install event in progress.');
   /* Using event.waitUntil(p) blocks the installation process on the provided
   promise. If the promise is rejected, the service worker won't be installed.
   */
@@ -58,7 +57,7 @@ self.addEventListener("install", function(event) {
         return cache.addAll(offlineFundamentals);
       })
       .then(function() {
-        console.log('WORKER: install completed');
+        // console.log('WORKER: install completed');
       })
   );
 });
@@ -69,7 +68,7 @@ self.addEventListener("install", function(event) {
    CSS resources, fonts, any images, etc.
 */
 self.addEventListener("fetch", function(event) {
-  console.log('WORKER: fetch event in progress.');
+  // console.log('WORKER: fetch event in progress.');
 
   /* We should only cache GET requests, and deal with the rest of method in the
      client-side, by handling failed POST,PUT,PATCH,etc. requests.
@@ -78,7 +77,7 @@ self.addEventListener("fetch", function(event) {
     /* If we don't block the event as shown below, then the request will go to
        the network as usual.
     */
-    console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+    // console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
     return;
   }
   /* Similar to event.waitUntil in that it blocks the fetch event on a promise.
@@ -110,7 +109,7 @@ self.addEventListener("fetch", function(event) {
         /* We return the cached response immediately if there is one, and fall
            back to waiting on the network as usual.
         */
-        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+        // console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
         return cached || networked;
 
         function fetchedFromNetwork(response) {
@@ -119,7 +118,7 @@ self.addEventListener("fetch", function(event) {
           */
           var cacheCopy = response.clone();
 
-          console.log('WORKER: fetch response from network.', event.request.url);
+          // console.log('WORKER: fetch response from network.', event.request.url);
 
           caches
             // We open a cache to store the response for this request.
@@ -132,7 +131,7 @@ self.addEventListener("fetch", function(event) {
               cache.put(event.request, cacheCopy);
             })
             .then(function() {
-              console.log('WORKER: fetch response stored in cache.', event.request.url);
+              // console.log('WORKER: fetch response stored in cache.', event.request.url);
             });
 
           // Return the response so that the promise is settled in fulfillment.
@@ -155,7 +154,7 @@ self.addEventListener("fetch", function(event) {
              - Generate a Response programmaticaly, as shown below, and return that.
           */
 
-          console.log('WORKER: fetch request failed in both cache and network.');
+          // console.log('WORKER: fetch request failed in both cache and network.');
 
           /* Here we're creating a response programmatically. The first parameter is the
              response body, and the second one defines the options for the response.
@@ -182,7 +181,7 @@ self.addEventListener("activate", function(event) {
   /* Just like with the install event, event.waitUntil blocks activate on a promise.
      Activation will fail unless the promise is fulfilled.
   */
-  console.log('WORKER: activate event in progress.');
+  // console.log('WORKER: activate event in progress.');
 
   event.waitUntil(
     caches
@@ -207,7 +206,7 @@ self.addEventListener("activate", function(event) {
         );
       })
       .then(function() {
-        console.log('WORKER: activate completed.');
+        // console.log('WORKER: activate completed.');
       })
   );
 });
